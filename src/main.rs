df7facc7078b8647c1f4ac118add25905e018c10
ckyeon <qsky8520@gmail.com>
2022-06-21 12:50:18 +0900

    Add [use gluesql] for OpenSource Contriubution Arcademy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use gluesql::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let storage = SledStorage::new("data/doc-db").unwrap();
+    let mut glue = Glue::new(storage);
+    let sqls = vec![
+        "CREATE TABLE Contributor (name TEXT);",
+        "INSERT INTO Contributor VALUES ('조기연');",
+        "SELECT * FROM Contributor;",
+    ];
+    
+    for sql in sqls {
+        let output = glue.execute(sql).unwrap();
+        println!("{:?}", output)
+    }
 }
